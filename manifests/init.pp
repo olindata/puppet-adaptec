7cdb9be8bbb28c48f601392e8d97045420140406
Srikrishna Das <krish@tribily.com>
2012-01-13 21:31:01 +0530

    disabling code for now - incomplete

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,14 +19,14 @@ class adaptec {
 #		ensure	=> absent,
 #  }
 
-	# Re-install new zabbix_agentd.conf
-	#
-  exec {  
-    "install dns UserParams" :
-      command =>
-			"cp /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf.bkup.$::mytimestamp; sed -i '/^UserParameter=adpt./d' /etc/zabbix/zabbix_agentd.conf",
-      path => ["/bin", "/usr/bin", "/sbin", "/usr/sbin"],
-  }
+#	# Re-install new zabbix_agentd.conf
+#	#
+#  exec {  
+#    "install dns UserParams" :
+#      command =>
+#			"cp /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf.bkup.$::mytimestamp; sed -i '/^UserParameter=adpt./d' /etc/zabbix/zabbix_agentd.conf",
+#      path => ["/bin", "/usr/bin", "/sbin", "/usr/sbin"],
+#  }
 
 
 
