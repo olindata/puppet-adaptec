d186c0d964ea2a07255f9d73dfcb1a80cf1e335c
Srikrishna Das <krish@tribily.com>
2012-01-13 20:22:15 +0530

    added Userparams file, adaptec script patched up from zabbixtemplates repo, disabled script creation, cron creation and enabled agent config backup for test

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,36 @@
 class adaptec {
 
-  file { "/opt/tribily/bin/adaptec_status.sh":
-    ensure  => present,
-    require => [File["/opt/tribily/bin"], Package["arcconf"]],
-    source  => "puppet:///adaptec/adaptec_status.sh",
-    owner  => "root", 
-    group  => "root", 
-    mode  => "0655",    
+#  file { "/opt/tribily/bin/adaptec_status.sh":
+#    ensure  => present,
+#    require => [File["/opt/tribily/bin"], Package["arcconf"]],
+#    source  => "puppet:///adaptec/adaptec_status.sh",
+#    owner  => "root", 
+#    group  => "root", 
+#    mode  => "0655",    
+#  }
+
+#	# CronJob for Monitoring
+#	#  
+#  cron { "tribily-adaptec":
+#    command  => "sh /opt/tribily/bin/adaptec_status.sh > /dev/null 2>&1",
+#    user  => "root", 
+#    minute  => "*/4",
+#    require  => File["/opt/tribily/bin/adaptec_status.sh"],
+#		ensure	=> absent,
+#  }
+
+time = Time.new
+mytimestamp = puts time.strftime("%Y%m%d_%H%M%S")
+
+	# Re-install new zabbix_agentd.conf
+	#
+  exec {  
+    "install dns UserParams" :
+      command =>
+			"cp /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf.bkup.${mytimestamp}; sed -i '/^UserParameter=adpt./d' /etc/zabbix/zabbix_agentd.conf",
+      path => ["/bin", "/usr/bin", "/sbin", "/usr/sbin"],
   }
 
-	# CronJob for Monitoring
-	#  
-  cron { "tribily-adaptec":
-    command  => "sh /opt/tribily/bin/adaptec_status.sh > /dev/null 2>&1",
-    user  => "root", 
-    minute  => "*/4",
-    require  => File["/opt/tribily/bin/adaptec_status.sh"],
-  
-  }
 
 
 }
