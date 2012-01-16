c8640d3b3da6bd50e96a34f5a4f08a0d1d2fdbd8
Srikrishna Das <krish@tribily.com>
2012-01-17 00:13:16 +0530

    disabling adaptec module, need to continue code tomorrow

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,26 +19,26 @@ class adaptec {
 #		ensure	=> absent,
 #  }
 
-	# For config backup datetime
-  $mytimestamp = inline_template("<%= Time.new.strftime('%Y%m%d_%H%M%S') %>")
-
-	# Backup old zabbix_agentd.conf and clean current with any UserParam modification for adpt
-	#
-  exec {  
-    "Backing up existing tribily agent configuration":
-      command =>
-			"cp /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf.bkup.$mytimestamp; sed -i '/^UserParameter=adpt./d' /etc/zabbix/zabbix_agentd.conf",
-			unless	=> "grep -i 'Tribily Role - Adaptec Extended Monitoring' /etc/zabbix/zabbix_agentd.conf",
-      path => ["/bin", "/usr/bin", "/sbin", "/usr/sbin"],
-  }
-
-
-	# Add new user Params
-	addparams {
-		"install dns Userparams":
-			ensure	=> present,
-			file	=>	"/etc/zabbix/zabbix_agentd.conf",
-	}
+#	# For config backup datetime
+#  $mytimestamp = inline_template("<%= Time.new.strftime('%Y%m%d_%H%M%S') %>")
+#
+#	# Backup old zabbix_agentd.conf and clean current with any UserParam modification for adpt
+#	#
+#  exec {  
+#    "Backing up existing tribily agent configuration":
+#      command =>
+#			"cp /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf.bkup.$mytimestamp; sed -i '/^UserParameter=adpt./d' /etc/zabbix/zabbix_agentd.conf",
+#			unless	=> "grep -i 'Tribily Role - Adaptec Extended Monitoring' /etc/zabbix/zabbix_agentd.conf",
+#      path => ["/bin", "/usr/bin", "/sbin", "/usr/sbin"],
+#  }
+#
+#
+#	# Add new user Params
+#	addparams {
+#		"install dns Userparams":
+#			ensure	=> present,
+#			file	=>	"/etc/zabbix/zabbix_agentd.conf",
+#	}
 
 
 }
