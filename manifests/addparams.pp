dcbba966f43ab9753047e2af9b7f498dc8b69e31
Srikrishna Das <krish@tribily.com>
2012-01-18 22:00:43 +0530

    content comes from template, file to use content type not source

--- manifests/addparams.pp
+++ manifests/addparams.pp
@@ -6,9 +6,6 @@ define adaptec::addparams (
   $ensure = present
 ) {
 
-  # Directory containing file parts
-  $dir = "/etc/zabbix/conf.d"
-
 	case $ensure {
 		'present': {
 			file { "/etc/zabbix/conf.d":
@@ -18,7 +15,7 @@ define adaptec::addparams (
 			}
 		  file { "/etc/zabbix/conf.d/tribily_dns_userparams.erb":
 		    ensure  => present,
-		    source  => template("adaptec/userparams.erb"),
+		    content  => template("adaptec/userparams.erb"),
 		    owner  => "root", 
 		    group  => "root", 
 		  }
