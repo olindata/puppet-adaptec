219404fd685ff290aacf69044fb31afc8d9aa666
Walter Heck <walter@tribily.com>
2012-06-02 12:26:37 +0800

    updated adaptec module to use https://github.com/Tribily/puppet-apt instead of manual deb package

--- manifests/raid.pp
+++ manifests/raid.pp
@@ -9,34 +9,19 @@ class adaptec::raid {
     false:     { notice 'no monitoring for adaptec::raid' }
   }
 
-  # Make sure the .deb is available locally on each server
-  file { '/opt/oplexing/debs/arcconf_6.50.18570-1_amd64.deb':
-    owner   => root,
-    group   => root,
-    mode    => 644,
-    ensure  => present,
-    require => File['/opt/oplexing/debs'],
-    source  => 'puppet:///adaptec/opt/oplexing/debs/arcconf_6.50.18570-1_amd64.deb'
-  }
-
-  # install prereqs for arcconf  and other stuff for the RAID array
-  package { 'libstdc++5':
-    ensure => present
-  }
-
   # ensure that the kernel module to monitor the hard drives is loaded
   kern_module { 'sg':
-    ensure => present 
+    ensure => present
  }
 
   # install Adaptec RAID controller software
   # arcconf package from http://hwraid.le-vert.net/wiki/DebianPackages
   # they have a repo, but it's unsigned so we use the deb directly
+  include apt::repo::hwraid_le_vert
+
   package { 'arcconf':
-    provider => dpkg,
-    ensure => latest,
-    source => '/opt/oplexing/debs/arcconf_6.50.18570-1_amd64.deb',
-    require => [File['/opt/oplexing/debs/arcconf_6.50.18570-1_amd64.deb'], Package['libstdc++5']]
+    ensure  => latest,
+    require => Apt::Repo::Hwraid_le_vert
   }
 
   # TODO
